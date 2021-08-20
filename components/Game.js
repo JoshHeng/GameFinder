e39062f94739ef9f092ab78852825001d7ccd58e
Josh Heng <josh@joshheng.co.uk>
2021-08-20 23:50:07 +0100

    feat: Add colour to games

--- components/Game.js
+++ components/Game.js
@@ -2,16 +2,14 @@ import Image from 'next/image'
 
 export default function Game({ game }) {
 	return (
-		<div className="bg-gray-100 rounded-xl m-1">
+		<div className="bg-gradient-to-tr from-yellow-400 to-red-500 rounded-xl m-1 shadow-lg">
 			{ game.image && <Image src={`/images/${game.image}`} width={game.imageWidth} height={game.imageHeight} alt="Cover image of game" className="rounded-t-lg" /> }
 
 			<div className="p-2">
-				<h3 className="text-lg font-bold text-blue-700">{ game.name }</h3>
+				<div className="text-gray-600 text-xs font-bold">{ game.pack }</div>
+				<h3 className="text-lg font-bold">{ game.name }</h3>
 				<blockquote className="font-medium">{ game.summary }</blockquote>
-
-				<div className="text-gray-600">{ game.pack }</div>
 			</div>
-			
 		</div>
 	)
 }
