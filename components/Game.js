9e537f58742c56d8cae238fb81fdd6970bdf4954
Josh Heng <josh@joshheng.co.uk>
2021-08-21 01:33:08 +0100

    game: Add gartic phone and skribblio and update colours

--- components/Game.js
+++ components/Game.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Game({ game }) {
 	return (
-		<div className={`bg-gradient-to-tr rounded-xl m-1 shadow-lg flex-auto w-64 max-w-md ${game.pack.gradient}`}>
+		<div className={`bg-gradient-to-tr rounded-xl m-1 shadow-lg flex-auto w-64 max-w-md ${game.gradient || game.pack.gradient || 'from-purple-400 to-yellow-400'}`}>
 			{ game.image && <Image src={game.image.src} width={game.image.width} height={game.image.height} alt="Cover image of game" className="rounded-t-lg" /> }
 
 			<div className="p-2">
