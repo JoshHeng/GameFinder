28f3f059a2ec642e833803296fc2378481929440
Josh Heng <josh@joshheng.co.uk>
2021-08-21 17:35:51 +0100

    feat: Add game statistics (e.g. player count)

--- components/Game.js
+++ components/Game.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-import { Heading, Text, Box } from '@chakra-ui/react';
+import { Heading, Text, Box, List, ListItem, ListIcon } from '@chakra-ui/react';
+import{ FiUsers, FiClock, FiSmile } from 'react-icons/fi'
 
 export default function Game({ game }) {
 	return (
-		<Box borderWidth="1px" borderRadius="lg" boxShadow="lg" w="64" maxW="md" m="1" overflow="hidden" bgGradient={game.gradient || game.pack.gradient || 'linear(to-tr, green.300, blue.500, purple.600)'} flexGrow="1" flexShrink="1" flexBasis="auto">
+		<Box borderWidth="1px" borderRadius="lg" boxShadow="lg" w="64" maxW="md" m="1" overflow="hidden" bgGradient={game.gradient || game.pack.gradient || 'linear(to-tr, green.300, blue.500, purple.600)'} flexGrow="1" flexShrink="1" flexBasis="auto" flexDir="column" style={{ display: 'flex' }}>
 			<Image src={game.image.src} width={game.image.width} height={game.image.height} alt="Cover image of game" />
 
 			<Box p="2">
@@ -11,6 +12,21 @@ export default function Game({ game }) {
 				<Heading as="h3" size="md" fontWeight="bold">{ game.name }</Heading>
 				<Text fontWeight="medium">{ game.description }</Text>
 			</Box>
+
+			<List m="2" p="3" spacing="1" mt="auto">
+				<ListItem>
+					<ListIcon as={FiUsers} />
+					<Text as="span" fontWeight="bold">Players: </Text>{game.minPlayers}-{game.maxPlayers}
+				</ListItem>
+				<ListItem>
+					<ListIcon as={FiClock} />
+					<Text as="span" fontWeight="bold">Duration: </Text>{game.duration || 'Variable'}
+				</ListItem>
+				<ListItem>
+					<ListIcon as={FiSmile} />
+					<Text as="span" fontWeight="bold">Family Friendly: </Text>{game.familyFriendly === true ? 'Yes' : (game.familyFriendly === 'optional' ? 'Optional' : 'No')}
+				</ListItem>
+			</List>
 		</Box>
 	);
 }
